31d148e9f478d663e64d74f61814b37ad08ff244
Michael Zheng <mzheng@Michaels-MacBook-Pro.local>
2024-02-13 18:00:27 -0500

    feb 12 activities completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './qcomps/profile_mistake.js'
+import FeedbackForm from './qcomps/thankYouCrash.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <FeedbackForm />
     </div>
   )
 }
