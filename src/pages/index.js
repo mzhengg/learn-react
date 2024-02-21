bddd7840db047ac4ecfda283ff01665763ce2a5b
Michael Zheng <mzheng@Michaels-MacBook-Pro.local>
2024-02-21 15:31:02 -0500

    feb 21 quizzes completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Scoreboard from './qcomps/updObjectsForm.js'
+import BucketList from './qcomps/arrObj.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Scoreboard />
+        <BucketList />
     </div>
   )
 }
